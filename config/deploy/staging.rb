f4b26e36c008c42b859cc376b0cd79e46825a491
Alfredo Uribe <fellou89@cloudspace.com>
2014-07-30 21:33:26 -0400

    vagrant up and bundle

---
+++ config/deploy/staging.rb
@@ -0,0 +1,64 @@
+host = 'staging.whatsup.com'
+environment = 'staging'
+
+role :app, [host]
+role :web, [host]
+role :db,  [host]
+
+set :branch, environment
+set :rails_env, environment
+set :stage, environment
+
+set :default_environment, 'RAILS_ENV' => environment
+set :application, host
+set :deploy_to, "/srv/www/#{host}"
+
+namespace :deploy do
+  task :start do               
+    on roles(:app) do          
+      execute "cd /srv/www/#{host}/current && "\
+      "bundle exec unicorn -E #{environment} -c /etc/unicorn/#{host}.rb -D"
+    end
+  end
+
+  task :stop do                
+    on roles(:app) do          
+      execute "kill -QUIT $(cat /srv/www/#{host}/unicorn.pid)"
+    end
+  end
+
+  task :restart do             
+    on roles(:app) do          
+      execute "kill -USR2 $(cat /srv/www/#{host}/unicorn.pid)"
+    end
+  end
+end
+
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
