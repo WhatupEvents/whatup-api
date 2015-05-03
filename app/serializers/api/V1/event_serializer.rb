798b1d9ecb25ab8e5d84b88f71a4c9c20dd35804
Alfredo Uribe <fellou89@cloudspace.com>
2015-05-03 13:15:47 -0400

    fixed serializer typo

--- app/serializers/api/V1/event_serializer.rb
+++ app/serializers/api/V1/event_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :start_time, :description, :address, :symbol_id, :public, :created_by_id
-  has_many :participants, serializer: ParticipantSerializer
+  has_many :participants, each_serializer: ParticipantSerializer
 end
