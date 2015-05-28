c518b62b1e7f8d3a93d06263dcab62dd1f5e6dfc
Alfredo Uribe <fellou89@cloudspace.com>
2015-05-28 17:19:37 -0400

    first try for updating rails code

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,3 +3,10 @@ require 'resque/server'
 Resque::Server.use(Rack::Auth::Basic) do |_user, password|
   password == ENV['RESQUE_ADMIN_PASSWORD']
 end
+
+unless Rails.application.config.cache_classes
+  Resque.after_fork do |job|
+    ActionDispatch::Reloader.cleanup!
+    ActionDispatch::Reloader.prepare!
+  end
+end
