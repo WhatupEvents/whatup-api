bb3cd2fa6d200e30b939f4e9c2a05687b59d7adb
Cloudspace <cloudspace@Joshs-MacBook-Pro-2.local>
2015-05-28 20:43:28 -0400

    trying to fix gcm notification

--- app/jobs/gcm_message_job.rb
+++ app/jobs/gcm_message_job.rb
@@ -2,7 +2,8 @@ class GcmMessageJob
   @queue = :messages
 
   def self.perform(event_id, recipient_ids)
-    GCM.new("AIzaSyBLDCdpQ9XBB9e-ecMI8OIQ_0pRJtd_kjg").send(
+    gcm = GCM.new("AIzaSyBLDCdpQ9XBB9e-ecMI8OIQ_0pRJtd_kjg")
+    gcm.send_notification(
       Device.where(user_id: recipient_ids).map(&:registration_id), 
       data: { event_id: event_id })
   end
