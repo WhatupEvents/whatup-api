f16634cfe29f0633028148de4e67c7cd4710009b
Cloudspace <cloudspace@Joshs-MacBook-Pro-2.local>
2015-05-28 20:23:56 -0400

    switched back to text from params for messages

--- app/controllers/api/v1/gcm_controller.rb
+++ app/controllers/api/v1/gcm_controller.rb
@@ -6,8 +6,7 @@ class Api::V1::GcmController < Api::V1::ApiController
   def message
     message = Message.create(sender_id: current_user.id, 
                              event_id: message_params["event_id"], 
-                             text: Resque.redis.namespace)
-#                             text: message_params["text"])
+                             text: message_params["text"])
     recipient_ids = (message.event.participants - [current_user]).map(&:id)
     Resque.enqueue(GcmMessageJob, message.event_id, recipient_ids)
   end
