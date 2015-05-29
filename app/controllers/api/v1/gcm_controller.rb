5882c9f842fbe191eb0e48138f9ca9155dcfd49b
Cloudspace <cloudspace@Joshs-MacBook-Pro-2.local>
2015-05-28 20:19:24 -0400

    updated job message is created in controller now

--- app/controllers/api/v1/gcm_controller.rb
+++ app/controllers/api/v1/gcm_controller.rb
@@ -4,7 +4,12 @@ class Api::V1::GcmController < Api::V1::ApiController
   doorkeeper_for :all
 
   def message
-    Resque.enqueue(GcmMessageJob, message_params, current_user.id)
+    message = Message.create(sender_id: current_user.id, 
+                             event_id: message_params["event_id"], 
+                             text: Resque.redis.namespace)
+#                             text: message_params["text"])
+    recipient_ids = (message.event.participants - [current_user]).map(&:id)
+    Resque.enqueue(GcmMessageJob, message.event_id, recipient_ids)
   end
   
   private
--- app/jobs/gcm_message_job.rb
+++ app/jobs/gcm_message_job.rb
@@ -1,23 +1,9 @@
 class GcmMessageJob
   @queue = :messages
 
-  def initialize(message_params, sender)
-    @message_params = message_params
-    @sender = sender
-  end
-
-  def self.perform(message_params, sender_id)
-    @message_params = message_params
-    @sender = User.find(sender_id)
-
-    message = Message.create(sender_id: @sender.id, 
-                             event_id: @message_params["event_id"], 
-                             text: Resque.redis.namespace)
-#                             text: @message_params["text"])
-
-    recipient_ids = (message.event.participants - [@sender]).map(&:id)
+  def self.perform(event_id, recipient_ids)
     GCM.new("AIzaSyBLDCdpQ9XBB9e-ecMI8OIQ_0pRJtd_kjg").send(
       Device.where(user_id: recipient_ids).map(&:registration_id), 
-      data: { event_id: message.event_id })
+      data: { event_id: event_id })
   end
 end
