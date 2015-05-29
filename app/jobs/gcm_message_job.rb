04f48cc6cd331cd3716465152029b84c30c7ef3d
Cloudspace <cloudspace@Joshs-MacBook-Pro-2.local>
2015-05-28 20:47:58 -0400

    trying to force wait for response

--- app/jobs/gcm_message_job.rb
+++ app/jobs/gcm_message_job.rb
@@ -3,8 +3,9 @@ class GcmMessageJob
 
   def self.perform(event_id, recipient_ids)
     gcm = GCM.new("AIzaSyBLDCdpQ9XBB9e-ecMI8OIQ_0pRJtd_kjg")
-    gcm.send_notification(
+    response = gcm.send_notification(
       Device.where(user_id: recipient_ids).map(&:registration_id), 
       data: { event_id: event_id })
+    Rails.logger.info response.to_s
   end
 end
