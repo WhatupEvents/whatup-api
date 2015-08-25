072c94029ae100c2eaf044318b520ba08e039caa
Cloudspace <cloudspace@Joshs-MacBook-Pro.local>
2015-08-24 22:16:22 -0400

    fixed serializer parent

--- app/serializers/api/v1/user_with_friends_serializer.rb
+++ app/serializers/api/v1/user_with_friends_serializer.rb
@@ -1,4 +1,4 @@
-class Api::V1::UserWithFriendsSerializer < UserSerializer
+class Api::V1::UserWithFriendsSerializer < Api::V1::UserSerializer
   has_many :friends, serializer: Api::V1::FriendSerializer
   has_many :friend_groups, serializer: Api::V1::FriendGroupSerializer
 end
