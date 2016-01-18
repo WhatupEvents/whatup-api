3b44ff471cc50ea429eef7ddf29b466d7e41e34d
Cloudspace <fellou89@gmail.com>
2016-01-18 14:34:49 -0500

    shout id serialized

--- app/serializers/api/v1/shout_serializer.rb
+++ app/serializers/api/v1/shout_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ShoutSerializer < ActiveModel::Serializer
-  attributes :text, :created_at, :user_id, :event_id, :source, :url
+  attributes :id, :text, :created_at, :user_id, :event_id, :source, :url
 
   def url
     object.image.url
