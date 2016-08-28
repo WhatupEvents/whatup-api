578c397f0f129d049a9ca5b7313b48b4b26b6cef
Cloudspace <fellou89@gmail.com>
2016-08-28 18:27:03 -0400

    added missing values for status

--- app/serializers/api/v1/friend_serializer.rb
+++ app/serializers/api/v1/friend_serializer.rb
@@ -3,7 +3,8 @@ class Api::V1::FriendSerializer < Api::V1::UserSerializer
 
   def status
     current_status = object.statuses.current.last
-    return Status.new(user_id: object.id) unless current_status
+    # may need to correct these values
+    return Status.new(user_id: object.id, symbol_id: 1, text: "", ups: 0) unless current_status
     current_status
   end
 end
