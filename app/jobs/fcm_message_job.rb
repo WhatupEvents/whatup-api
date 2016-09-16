82e991a32e903e825f29bde07d9d9e1a640a4595
Cloudspace <fellou89@gmail.com>
2016-09-16 00:19:48 -0400

    back to server api, iterating over reg ids

--- app/jobs/fcm_message_job.rb
+++ app/jobs/fcm_message_job.rb
@@ -3,18 +3,23 @@ class FcmMessageJob
 
   def self.perform(data, recipient_ids)
     #TODO: gotta move this into an environment file
-    # fcm = FCM.new("AIzaSyAlSPpjCGewFMOB58ExE8PmHxy7aje4D8w")
 
-    fcm = FCM.new("AIzaSyDkhn2fkoHTzghKXo-Jhkkofoq5Q_kRYtQ")
-    resp = fcm.send(
-      Device.where(user_id: recipient_ids, os: "iOS").map(&:registration_id), 
-      data)
-    Rails.logger.info resp.to_s
+    fcm = FCM.new("AIzaSyAlSPpjCGewFMOB58ExE8PmHxy7aje4D8w")
+    Device.where(user_id: recipient_ids).map(&:registration_id).each do |reg_id|
+      resp = fcm.send(reg_id, {data: data})
+      Rails.logger.info resp.to_s
+    end
 
-    fcm = FCM.new("AIzaSyDrZ95JxylkKXiV7MZIvp5KyEnFOQW04is")
-    resp = fcm.send(
-      Device.where(user_id: recipient_ids, os: "Android").map(&:registration_id), 
-      data)
-    Rails.logger.info resp.to_s
+    # fcm = FCM.new("AIzaSyDkhn2fkoHTzghKXo-Jhkkofoq5Q_kRYtQ")
+    # resp = fcm.send(
+    #   Device.where(user_id: recipient_ids, os: "iOS").map(&:registration_id), 
+    #   data)
+    # Rails.logger.info resp.to_s
+
+    # fcm = FCM.new("AIzaSyDrZ95JxylkKXiV7MZIvp5KyEnFOQW04is")
+    # resp = fcm.send(
+    #   Device.where(user_id: recipient_ids, os: "Android").map(&:registration_id), 
+    #   data)
+    # Rails.logger.info resp.to_s
   end
 end
