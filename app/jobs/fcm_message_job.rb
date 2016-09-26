82edbf7dc158551fc2920300e0aad011674170e4
Cloudspace <fellou89@gmail.com>
2016-09-26 14:48:34 -0400

    uniq registration id fcms

--- app/jobs/fcm_message_job.rb
+++ app/jobs/fcm_message_job.rb
@@ -4,7 +4,7 @@ class FcmMessageJob
   def self.perform(data, recipient_id)
     #TODO: gotta move this into an environment file
     fcm = FCM.new("AIzaSyAlSPpjCGewFMOB58ExE8PmHxy7aje4D8w")
-    Device.where(user_id: recipient_id).map(&:registration_id).each do |reg_id|
+    Device.where(user_id: recipient_id).map(&:registration_id).uniq.each do |reg_id|
       resp = fcm.send_with_notification_key(reg_id, {
         notification: {title: data['event_name'], body: "#{data['event_name']} message"},
         data: {event_id: data['event_id']},
