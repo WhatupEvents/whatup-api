3691af65c08a363164ed02d937d9febff99b0a6d
Cloudspace <fellou89@gmail.com>
2016-09-30 13:24:09 -0400

    removing tag to debug ios

--- app/jobs/fcm_message_job.rb
+++ app/jobs/fcm_message_job.rb
@@ -5,8 +5,9 @@ class FcmMessageJob
     #TODO: gotta move this into an environment file
     fcm = FCM.new("AIzaSyAlSPpjCGewFMOB58ExE8PmHxy7aje4D8w")
     Device.where(user_id: recipient_id).map(&:registration_id).uniq.each do |reg_id|
+      # , tag: "#{data['event_id']}"
       resp = fcm.send_with_notification_key(reg_id, {
-        notification: {title: data['event_name'], body: "#{data['event_name']} message", tag: "#{data['event_id']}"},
+        notification: {title: data['event_name'], body: "#{data['event_name']} message"},
         data: {event_id: data['event_id']},
         content_available: true,
         priority: "high"
