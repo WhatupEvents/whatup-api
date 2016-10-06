ba068c5d382774e182d4670e4a42277d0453191f
Cloudspace <fellou89@gmail.com>
2016-10-05 20:54:20 -0400

    updated shout serializer

--- app/serializers/api/v1/shout_serializer.rb
+++ app/serializers/api/v1/shout_serializer.rb
@@ -1,7 +1,11 @@
 class Api::V1::ShoutSerializer < ActiveModel::Serializer
-  attributes :id, :text, :created_at, :user_id, :event_id, :source, :url
+  attributes :id, :text, :created_at, :shouter, :event_id, :source, :url
 
   def url
     object.image.url
+  end
+
+  def sender
+    User.find(object.user_id).name
   end
 end
