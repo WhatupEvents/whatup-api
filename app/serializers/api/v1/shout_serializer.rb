4f9cb604136db17fc1618c3eaefbe43cf2b8628b
Cloudspace <fellou89@gmail.com>
2016-10-05 22:12:10 -0400

    added user_id back

--- app/serializers/api/v1/shout_serializer.rb
+++ app/serializers/api/v1/shout_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ShoutSerializer < ActiveModel::Serializer
-  attributes :id, :text, :created_at, :shouter, :event_id, :source, :url
+  attributes :id, :text, :created_at, :shouter, :user_id, :event_id, :source, :url
 
   def url
     object.image.url
