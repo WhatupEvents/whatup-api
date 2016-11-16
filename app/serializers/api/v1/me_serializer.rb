5422d561d6d9f916246637a5ee53ec2d37fccb67
Alfredo Uribe <alfredo@cloudspace.com>
2016-11-15 23:09:07 -0500

    adding function for attributes

--- app/serializers/api/v1/me_serializer.rb
+++ app/serializers/api/v1/me_serializer.rb
@@ -1,4 +1,12 @@
 class Api::V1::MeSerializer < ActiveModel::Serializer
   has_one :user, serializer: Api::V1::UserSerializer
   has_one :access_token, serializer: Api::V1::AccessTokenSerializer 
+
+  def access_token
+    attributes[:access_token]
+  end
+
+  def user
+    attributes[:user]
+  end
 end
