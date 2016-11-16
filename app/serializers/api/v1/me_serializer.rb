a2203f080f3beec1d8abf893f47d20acf7cf090a
Alfredo Uribe <alfredo@cloudspace.com>
2016-11-15 23:36:35 -0500

    testing keys

--- app/serializers/api/v1/me_serializer.rb
+++ app/serializers/api/v1/me_serializer.rb
@@ -3,7 +3,7 @@ class Api::V1::MeSerializer < ActiveModel::Serializer
   has_one :access_token, serializer: Api::V1::AccessTokenSerializer 
 
   def access_token
-    object.attributes['access_token']
+    object.attributes[:access_token]
   end
 
   def user
