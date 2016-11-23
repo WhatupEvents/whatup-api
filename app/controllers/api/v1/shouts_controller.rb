da75fd5cf8dab22d76b8080809a95cab29cbaea2
Alfredo Uribe <alfredo@cloudspace.com>
2016-11-23 00:17:48 -0500

    using created_at instead of shout id

--- app/controllers/api/v1/shouts_controller.rb
+++ app/controllers/api/v1/shouts_controller.rb
@@ -24,11 +24,10 @@ class Api::V1::ShoutsController < Api::V1::ApiController
   private
 
   def render_shouts
-    last = params.has_key?(:last_id) ? Shout.find(params.delete(:last_id)) : nil
-    last_id = last ? last.id : 0
+    last = params.has_key?(:last_id) ? Shout.find(params.delete(:last_id)) : Shout.last
     shouts = Shout.where('created_at > ?', Time.now-7.hour)
       .where(event_id: Event.current.pub.near_user(current_user, 2.0).map(&:id))
-      .where('id < ?', last_id)
+      .where('created_at < ?', last.created_at)
       .limit(15).order(created_at: :desc)
     if shouts.present?
       render json: shouts,
