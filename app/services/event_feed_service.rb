5f88077a152128bcaeb8ce04498bc127c9c8e912
Alfredo Uribe <alfredo@cloudspace.com>
2016-12-06 20:17:51 -0500

    category id for feed events

--- app/services/event_feed_service.rb
+++ app/services/event_feed_service.rb
@@ -26,8 +26,47 @@ class EventFeedService
         location: ev["location"],
         symbol_id: 1,
         latitude: "28.6024",
-        longitude: "-81.2001"
+        longitude: "-81.2001",
+        category_id: get_category_id(ev["categoru"])
       )
     end
+  end
+
+  def get_category_id(feed_category)
+    cat = 8 #"beer"
+    if feed_category == "Academic"
+      cat = 4 #"art"
+    elsif feed_category == "Arts Exhibit"
+      cat = 4 #"art"
+    elsif feed_category == "Carrer/Jobs"
+      cat = 1 #"fitness"
+    elsif feed_category == "Concert/Performance"
+      cat = 2 #"music"
+    elsif feed_category == "Entertainment"
+      cat = 2 #"music"
+    elsif feed_category == "Health"
+      cat = 5 #"volunteer"
+    elsif feed_category == "Holiday"
+      cat = 0 #"leisure"
+    elsif feed_category == "Meeting"
+      cat = 3 #"party"
+    elsif feed_category == "Open Forum"
+      cat = 5 #"volunteer"
+    elsif feed_category == "Recreation & Exercise"
+      cat = 1 #"fitness"
+    elsif feed_category == "Service/Volunteer"
+      cat = 5 #"volunteer"
+    elsif feed_category == "Social Event"
+      cat = 3 #"party"
+    elsif feed_category == "Speaker/Lecture/Seminar"
+      cat = 4 #"art"
+    elsif feed_category == "Sports"
+      cat = 6 #"sports"
+    elsif feed_category == "Tour/Open House/Information Session"
+      cat = 5 #"volunteer"
+    elsif feed_category == "Workshop/Conference"
+      cat = 7 #"food"
+    end
+    return cat
   end
 end
