3ce870e4217aa238ef0f56ccfe1f305eb7ab3f01
Alfredo Uribe <alfredo@cloudspace.com>
2016-12-06 20:41:40 -0500

    fix for message images

--- app/serializers/api/v1/message_serializer.rb
+++ app/serializers/api/v1/message_serializer.rb
@@ -2,7 +2,7 @@ class Api::V1::MessageSerializer < ActiveModel::Serializer
   attributes :sender, :event_id, :text, :media, :source, :created_at, :url
 
   def url
-    return if object.image.url == "/images/original/missing.png"
+    object.image.url
   end
 
   def sender
