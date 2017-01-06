6495b111481d00a576fa7515b5d057c834ed164d
Alfredo Uribe <alfredo@cloudspace.com>
2017-01-05 23:37:53 -0500

    aws credentials with new s3 user

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,13 +7,14 @@ class Message < ActiveRecord::Base
     storage: :s3,
     bucket: "whatupevents-images",
     :s3_region => 'us-east-2',
+    s3_permissions: "public-read",
     s3_credentials: Proc.new{|p| p.instance.s3_credentials}
   do_not_validate_attachment_file_type :image
 
   def s3_credentials
     {
-     access_key_id: "AKIAIF5LMWPOUEEJMVCA",
-     secret_access_key: "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"
+     access_key_id: "AKIAJSKGHQFVPEXZZGMA",
+     secret_access_key: "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"
     }
   end
 
