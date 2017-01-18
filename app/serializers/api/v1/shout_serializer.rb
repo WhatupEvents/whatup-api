c304e07a072047738d75dea8d1caa890dc49e041
Alfredo Uribe <alfredo@cloudspace.com>
2017-01-17 22:49:38 -0500

    added blacklist code

--- app/serializers/api/v1/shout_serializer.rb
+++ app/serializers/api/v1/shout_serializer.rb
@@ -1,6 +1,16 @@
 class Api::V1::ShoutSerializer < ActiveModel::Serializer
   attributes :id, :text, :created_at, :shouter, :user_id, :event_id, :source, :url
 
+  def text
+    blacklist = ['fuck', 'shit', 'cunt']
+    blacklist.each do |b|
+      sub = ''
+      b.length.times{|i| sub+='*'}
+      object.text.gsub(b,sub) 
+    end
+    object.text
+  end
+
   def shouter
     User.find(object.user_id).name
   end
