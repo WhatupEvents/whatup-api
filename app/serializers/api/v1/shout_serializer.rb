3b12e8514b74ff7311bcf3cbd82d95f061070d39
Alfredo Uribe <alfredo@cloudspace.com>
2017-01-17 23:54:03 -0500

    fix where url should go
    better matching for obscenities

--- app/serializers/api/v1/shout_serializer.rb
+++ app/serializers/api/v1/shout_serializer.rb
@@ -2,11 +2,15 @@ class Api::V1::ShoutSerializer < ActiveModel::Serializer
   attributes :id, :text, :created_at, :shouter, :user_id, :event_id, :source, :url
 
   def text
-    blacklist = ['fuck', 'shit', 'cunt']
+    blacklist = [/[a]+[r]+[s]+[e]+/, /[a]+[s]+[s]+/, /[a]+[s]+[s]+[h]+[o]+[l]+[e]+/, /[b]+[a|e]+[s]+[t]+[a|e]+[r]+[d]+/, /[b]+[i|a]+[t]+[c]+[h]+/, /[b]+[a|o]+[l]+[o]+[c]+[k]+[s]+/, /child-.*/, /[c]+[r]+[a]+[p]+/, /[d]+[a|e|i]+[m]+[n]+/, /[g]+[o]+[d]+[a]+[m]+[n]+/, /[m]+[o]+[t]+[h]+[e]+[r]+[f]+[u|a|o]+[c]+[k]+[e]+[r]+/, /[n]+[i]+[g]+[a|e]+[r]*/, /[t]+[w]+[a]+[t]+/, /[w]+[h]+[o|a]+[r]+[e]+/, /[f]+[u|a|o]+[c]+[k]+[e]*[r]*/, /[s]+[h]+[i]+[t]+/, /[c]+[u]+[n]+[t]+/]
     blacklist.each do |b|
-      sub = ''
-      b.length.times{|i| sub+='*'}
-      object.text.gsub(b,sub) 
+      match = b.match(object.text)
+      if match
+        match = match[0]
+        sub = ''
+        match.length.times{|i| sub+='*'}
+        object.text.gsub(match,sub) 
+      end
     end
     object.text
   end
