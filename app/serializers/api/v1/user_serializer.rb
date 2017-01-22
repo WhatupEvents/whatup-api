c63e287fe082d127c815ed82ed49bd20c42d7eff
Alfredo Uribe <alfredo@cloudspace.com>
2017-01-22 18:44:27 -0500

    user updates
    add friend route
    creating users by user name instead of fb id
    only associate friends by fb ids when param is present
    added image to user
    serializing url for user profile pic

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,6 +1,12 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :role, :fb_id, :accepted_terms
+  attributes :id, :first_name, :last_name, :email, :role, :fb_id, :accepted_terms, :username, :url
   has_one :status, serializer: Api::V1::StatusSerializer
+
+  def url
+    unless object.image.url.include? "missing.png"
+      object.image.url.split('whatupevents-images/')[1].split('?')[0].gsub('/', '-').gsub('.','_')
+    end
+  end
 
   def status
     current_status = object.statuses.current.last
