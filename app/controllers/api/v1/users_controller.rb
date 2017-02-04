bad906b40bf150242e2b5960c680f082d973436c
Alfredo Uribe <alfredo@cloudspace.com>
2017-02-03 23:16:21 -0500

    controller to update accepted terms

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::UsersController < Api::V1::ApiController
       @current_user.save
       render_me :created
     else
+      @current_user.update_attribute('accepted_terms', user_params['accepted_terms'])
       @current_user.update_attribute('email', user_params['email'])
       render_me :ok
     end
@@ -62,7 +63,8 @@ class Api::V1::UsersController < Api::V1::ApiController
       :email, 
       :first_name, 
       :last_name, 
-      :fb_id
+      :fb_id,
+      :accepted_terms
     )
   end
 end
