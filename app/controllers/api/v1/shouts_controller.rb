4568b1fabe6594d4993653185ff7522e9d95b60c
Alfredo Uribe <alfredo@cloudspace.com>
2017-02-17 09:35:31 -0500

    fixed shout flag typo

--- app/controllers/api/v1/shouts_controller.rb
+++ app/controllers/api/v1/shouts_controller.rb
@@ -65,7 +65,7 @@ class Api::V1::ShoutsController < Api::V1::ApiController
     shouts = Shout.where('created_at > ?', Time.now-7.hour)
       .where(event_id: Event.current.pub.near_user(lat, long, 20.0).map(&:id))
       .where('created_at <= ?', last.created_at)
-      .where('flagged <= 3')
+      .where('flag <= 3')
       .limit(15).order(created_at: :desc)
     if shouts.present?
       render json: shouts,
