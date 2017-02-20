d770912e00583649757859c41b58ea21c22bb0a2
Alfredo Uribe <alfredo@cloudspace.com>
2017-02-20 17:39:16 -0500

    render shouts fix

--- app/controllers/api/v1/shouts_controller.rb
+++ app/controllers/api/v1/shouts_controller.rb
@@ -72,8 +72,8 @@ class Api::V1::ShoutsController < Api::V1::ApiController
     long, lat = get_geo.split(':')
     shouts = Shout.where('created_at > ?', Time.now-7.hour)
       .where(event_id: Event.current.pub.near_user(lat, long, 20.0).map(&:id))
-      .where('created_at <= ?', last.created_at)
-      .where('flag <= 3')
+      .where('created_at < ?', last.created_at)
+      .where('flag < 3')
       .limit(15).order(created_at: :desc)
     if shouts.present?
       render json: shouts,
