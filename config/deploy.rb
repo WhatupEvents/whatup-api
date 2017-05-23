4fda8089e309940ef756d046217cce8e7b34d97c
Alfredo Uribe <alfredo@cloudspace.com>
2017-05-23 00:20:28 -0400

    using ruby 2.3.3 because of fixnum bignum issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:fellou89/whatup-api.git'
 set :resque_environment_task, true
 set :workers, { messages: 3, low: 1 }
 
-set :rvm1_ruby_version, '2.4.0@global'
+set :rvm1_ruby_version, '2.3.3@global'
 # set :default_shell, '/bin/bash -l'
 
 # Default branch is :master
