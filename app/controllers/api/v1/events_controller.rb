50a1c163405b9b2756ec8a855c2019414f739e01
Alfredo Uribe <alfredo@cloudspace.com>
2017-05-28 15:29:42 -0400

    debugging friend_ids not clearing

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -38,13 +38,12 @@ class Api::V1::EventsController < Api::V1::ApiController
     else
 
       # clear participants when going from private to public or viceversa
-      Rails.logger.info event.public
-      Rails.logger.info create_event_params
-      Rails.logger.info create_event_params[:public]
       if event.public != (create_event_params[:public] == "true")
         event.participants = []
         Rails.logger.info event.participants.count
-        create_event_params.delete("friend_ids")
+        Rails.logger.info create_event_params
+        create_event_params['friend_ids'] = []
+        create_event_params.delete('friend_ids')
         Rails.logger.info create_event_params
       end
 
