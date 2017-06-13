3000775444f671c1ee039ef51ed968c80380decf
Alfredo Uribe <alfredo@cloudspace.com>
2017-06-13 00:53:51 -0400

    fixed migration

--- db/migrate/20170613033734_add_unread_to_participant_relationships.rb
+++ db/migrate/20170613033734_add_unread_to_participant_relationships.rb
@@ -1,5 +1,5 @@
 class AddUnreadToParticipantRelationships < ActiveRecord::Migration
   def change
-    add_column :participant_relationships, :unread, :boolean
+    add_column :participant_relationships, :unread, :integer
   end
 end
