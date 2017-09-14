b13f11cab5b650c5326192b6a54640f9ab79cadf
Alfredo Uribe <alfredo@cloudspace.com>
2017-09-14 19:10:30 -0400

    modified new notification text

--- app/jobs/fcm_message_job.rb
+++ app/jobs/fcm_message_job.rb
@@ -53,7 +53,7 @@ class FcmMessageJob
           elsif data.has_key? 'status_text'
             # interested friend in status job
             resp = fcm.send_with_notification_key(reg_id, {
-              notification: {title: 'Status has been pinged!', body: "#{data['friend_name']} is interested in your status: '#{data['status_text']}'"},
+              notification: {title: 'Status was pinged!', body: "#{data['friend_name']} is interested in: '#{data['status_text']}'"},
               data: data,
               content_available: true,
               priority: "high"
