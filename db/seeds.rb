e384bbd58cae9ac0369ab9f31d154db190367c64
Alfredo Uribe <alfredo@cloudspace.com>
2017-10-06 19:24:37 -0400

    fixed categories seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,7 @@ if Rails.env.staging? || Rails.env.development?
     name:'android', redirect_uri: 'https://dev.whatsup.com',
     uid: "0a97a553e51126e08b024173facf767e87b944fc85ddb863529b9507845b114d"
   )
+end
 
 categories = ['Academic', 'Arts Exhibit', 'Career/Jobs', 'Concert/Performance', 'Entertainment', 'Health', 'Holiday', 'Meeting', 'Open Forum', 'Recreation & Exercise', 'Service/Volunteer', 'Social Event', 'Speaker/Lecture/Seminar', 'Sports', 'Tour/Open House/Information Session', 'Workshop/Conference', 'Uncategorized/Other']
 categories.each { |c| Category.find_or_create_by(name: c) }
-
-end
