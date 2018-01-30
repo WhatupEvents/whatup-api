34f7f9e5d8e818f73a93a8741e457471d2b3b149
Alfredo Uribe <alfredo@cloudspace.com>
2018-01-30 17:10:25 -0500

    getting right length for details attribute

--- db/migrate/20180130220315_add_emoticon_to_events.rb
+++ db/migrate/20180130220315_add_emoticon_to_events.rb
@@ -1,6 +1,6 @@
 class AddEmoticonToEvents < ActiveRecord::Migration
   def change
     execute "ALTER TABLE events CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    execute "ALTER TABLE events MODIFY details VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE events MODIFY details VARCHAR(65535) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
   end
 end
