411d213e58171b3ea2f52846d7335259a14209b5
Alfredo Uribe <alfredo@cloudspace.com>
2018-02-18 23:18:17 -0500

    serialize event create_by_type

--- app/serializers/api/v1/event_serializer.rb
+++ app/serializers/api/v1/event_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :start_time, :end_at, :details, :location, :latitude, :longitude, :url, :symbol_id,
-    :created_by_id, :created_at, :updated_at, :category_id, :public, :source, :image_updated_at, :notify
+    :created_by_id, :created_by_type, :created_at, :updated_at, :category_id, :public, :source, :image_updated_at, :notify
   has_many :participants, each_serializer: Api::V1::ParticipantSerializer
 
   def url
