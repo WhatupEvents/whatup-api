35a9f2710fdac9395cc5a6a8cf940cf034c2341d
Alfredo Uribe <alfredo@cloudspace.com>
2018-02-19 12:27:08 -0500

    foreign key for follow relationship fix

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -1,4 +1,4 @@
 class FollowRelationship < ActiveRecord::Base
   belongs_to :follower, class_name: 'User'
-  belongs_to :followed, class_name: 'Organization'
+  belongs_to :followed, class_name: 'Organization', foreign_key: :followed_id
 end
