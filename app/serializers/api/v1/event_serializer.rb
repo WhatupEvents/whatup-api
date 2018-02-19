915d1ea49e500eab25f3759941a96c23f202bc4a
Alfredo Uribe <alfredo@cloudspace.com>
2018-02-19 12:52:45 -0500

    serializing organization name for event

--- app/serializers/api/v1/event_serializer.rb
+++ app/serializers/api/v1/event_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_time, :end_at, :details, :location, :latitude, :longitude, :url, :symbol_id,
+  attributes :id, :name, :start_time, :end_at, :details, :location, :latitude, :longitude, :url, :symbol_id, :organization_name,
     :created_by_id, :created_by_type, :created_at, :updated_at, :category_id, :public, :source, :image_updated_at, :notify
   has_many :participants, each_serializer: Api::V1::ParticipantSerializer
 
@@ -12,5 +12,9 @@ class Api::V1::EventSerializer < ActiveModel::Serializer
   def notify
     participant = object.participant_relationships.select{|p| p.participant_id == serialization_options[:current_user]}[0]
     participant ? participant.notify : false
+  end
+
+  def organization_name
+    object.created_by_type == "Organization" ? object.created_by.name : ""
   end
 end
