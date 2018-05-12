a2888e4fbf5dc64c3f39b31647173b83d1e7842a
Alfredo Uribe <alfredo@cloudspace.com>
2018-05-12 17:34:02 -0400

    migration fixes

--- db/migrate/20180511164831_change_symbol_to_topic_on_events.rb
+++ db/migrate/20180511164831_change_symbol_to_topic_on_events.rb
@@ -1,7 +1,13 @@
 class ChangeSymbolToTopicOnEvents < ActiveRecord::Migration
   def change
     rename_column :events, :symbol_id, :topic_id
+
+    remove_index :topics, :symbol_id
     rename_column :topics, :symbol_id, :category_id
+    add_index :topics, :category_id
+
+    remove_index :statuses, :symbol_id
     rename_column :statuses, :symbol_id, :topic_id
+    add_index :statuses, :topic_id
   end
 end
