3c0011322575f7de1bc4d2ecef0b24df4b33f591
Alfredo Uribe <alfredo@cloudspace.com>
2018-05-16 12:24:26 -0400

    all users can pull organizations

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::OrganizationsController < Api::V1::ApiController
   doorkeeper_for :all
 
   def index
-    authorize Organization
     render json: current_user.organizations,
            each_serializer: Api::V1::OrganizationSerializer,
            status: :ok
