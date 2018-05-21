59269a478b4077f672aa08f68f100d7810fd827e
Alfredo Uribe <alfredo@cloudspace.com>
2018-05-21 16:49:11 -0400

    need all organizations for admin
    so they can edit any event

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::OrganizationsController < Api::V1::ApiController
   doorkeeper_for :all
 
   def index
-    render json: current_user.organizations,
+    render json: current_user.admin? ? Organization.all : current_user.organizations,
            each_serializer: Api::V1::OrganizationSerializer,
            status: :ok
   end
