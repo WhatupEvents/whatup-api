ee486705dbebc3c03dbacac6774b8635e58851aa
Alfredo Uribe <alfredo@cloudspace.com>
2018-05-23 19:29:35 -0400

    notification destroy

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -6,4 +6,12 @@ class Api::V1::NotificationsController < Api::V1::ApiController
            each_serializer: Api::V1::NotificationSerializer,
            status: :ok
   end
+
+  def destroy
+    @notification = Notification.find(params[:id])
+
+    @notification.destroy
+    render json: {},
+           status: :ok
+  end
 end
