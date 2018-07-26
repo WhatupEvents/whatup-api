2e11a1a22e9653f7a7df179654d86b4bf53cf500
Alfredo Uribe <alfredo@cloudspace.com>
2018-07-25 21:56:28 -0400

    missing foreign key

--- app/models/viewing_relationship.rb
+++ app/models/viewing_relationship.rb
@@ -1,6 +1,6 @@
 class ViewingRelationship < ActiveRecord::Base
   belongs_to :shout
-  belongs_to :viewer, class_name: 'User'
+  belongs_to :viewer, class_name: 'User', foreign_key: :user_id
 
   validates :shout, presence: true
   validates :viewer, presence: true
