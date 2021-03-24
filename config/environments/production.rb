dd8dd4c28a52136dad1e36a8c3b3b6d20db64ec5
Alfredo <alfredo.uribe@arthrex.com>
2021-03-23 22:29:45 -0400

    production logging try1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,7 @@ Whatup::Application.configure do
 
   # See everything in the log (default is :info)
   config.log_level = :debug
+  RAILS_DEFAULT_LOGGER = Logger.new('log/production.log')
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
