fdcf4bfdd6a64c9c14832eabb81caabfea265fea
Alfredo <alfredo.uribe@arthrex.com>
2021-05-13 20:32:00 -0400

    trying other logger configs

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Whatup::Application.initialize!
 
-Rails.logger = Logger.new(STDOUT)
-Rails.logger.level = Logger::DEBUG
+custom_logger = Logger.new(STDOUT)
+Rails.logger.extend(ActiveSupport::Logger.broadcast(custom_logger))
+custom_logger.debug('testing...................')
